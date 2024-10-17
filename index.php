3a7bdf1a62ae74283a6e0ab7f6b0d880b2957fe2
Eddy Martin <eddypmartin@gmail.com>
2024-10-17 11:44:01 -0400

    s2c2 segmentation de index.php

--- index.php
+++ index.php
@@ -4,58 +4,37 @@
  * index.php - Le modèle par défaut de wordpress
  */
 ?>
+<?php get_header() ?>
 
-<!DOCTYPE html>
-<html lang="fr">
-
-<head>
-  <meta charset="UTF-8" />
-  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-  <title>31W</title>
-  <?php wp_head(); ?>
-</head>
-
-<body>
-  <header class="entete">
-    <section class="global">
-      <h1>31W</h1>
-      <nav>
-        <ul>
-          <li><a href="#">Accueil</a></li>
-          <li><a href="#">À propos</a></li>
-          <li><a href="#">Contact</a></li>
-        </ul>
-      </nav>
-      <form class="recherche">
-        <input type="search" name="" id="" />
-        <img
-          src="https://s2.svgbox.net/hero-outline.svg?ic=search&color=000"
-          width="20"
-          height="20" />
-      </form>
-    </section>
-  </header>
-  <main class="principal">
-    <section class="global">
-      <h2>Accueil</h2>
+<main class="principal">
+  <section class="global">
+    <h2>Liste de cours</h2>
+    <div class="principal__conteneur">
       <?php if (have_posts()): ?>
         <?php while (have_posts()) :  the_post(); ?>
+          <?php
+          $chaine = get_the_title();
+          $sigle = substr($chaine, 0, 7);
+          $titre = substr($chaine, 8, strrpos($chaine, "(") - 8);
+          ?>
           <article class="principal__article">
-            <h2><?php the_title(); ?></h2>
+            <h5><?php echo $sigle ?></h5>
+            <h6><?php echo $titre ?></h6>
             <p><?php echo wp_trim_words(get_the_excerpt(), 20, null); ?></p>
           </article>
         <?php endwhile; ?>
-      <?php endif ?>
-    </section>
-  </main>
-  <footer class="pied">
-    <section class="global">
-      <div>1</div>
-      <div>2</div>
-      <div>3</div>
-    </section>
-  </footer>
-  <?php wp_footer(); ?>
+    </div>
+  <?php endif ?>
+  </section>
+</main>
+<footer class="pied">
+  <section class="global">
+    <div>1</div>
+    <div>2</div>
+    <div>3</div>
+  </section>
+</footer>
+<?php wp_footer(); ?>
 </body>
 
 </html>
